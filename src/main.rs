ccdc365cde8e95ed4b89fbf3fef51d82a9e24305
dougfort <doug.fort@gmail.com>
2022-06-09 14:37:45 -0400

    get a simple query working

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 use std::collections::HashMap;
+use std::fs;
+
+use hyper::HeaderMap;
+use hyper::header::AUTHORIZATION;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
+    let token = fs::read_to_string("/home/dougfort/.config/twitterv2/token")?;
+    let token = token.trim();
+    let mut headers = HeaderMap::new();
+    headers.insert(AUTHORIZATION,format!("Bearer {}",token).parse()?);
+            
+    let client = reqwest::Client::new();
+    let resp = client
+        .get("https://api.twitter.com/2/tweets/search/recent?query=aaa")
+        .headers(headers)
+        .send()
+        .await?   
+        .error_for_status()?     
+//        .json::<HashMap<String, String>>()
+        .text()
         .await?;
-    println!("{:#?}", resp);
+    println!("{:?}", resp);
     Ok(())
 }
